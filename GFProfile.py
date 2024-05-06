a82a54addee75211038a5b662167e3059b0a14e5
Paul Bone <paul@bone.id.au>
2024-05-06 23:21:03 +1000

    Costomize the profile so it can be used for bins too

--- GFProfile.py
+++ GFProfile.py
@@ -2,22 +2,27 @@
 from build123d import *
 from build123d.build_enums import Align, Mode
 from build123d.topology import Part, Solid
-from ocp_vscode import show, reset_show, set_port, set_defaults, get_defaults
+from ocp_vscode import show, reset_show, set_port, set_defaults, get_defaults, show_object
 set_port(3939)
 
 from parameters import *
 
 class GFProfile(BasePartObject):
-    def __init__(self, rotation: tuple[float, float, float] | Rotation = (0, 0, 0), align: Align | tuple[Align, Align, Align] = None, mode: Mode = Mode.ADD):
+    def __init__(self, base = True, rotation: tuple[float, float, float] | Rotation = (0, 0, 0), align: Align | tuple[Align, Align, Align] = None, mode: Mode = Mode.ADD):
+        my_bin_size = bin_size
+        my_outer_rad = outer_rad
+        if not base:
+             my_bin_size = 41.5
+             my_outer_rad = 7.5/2
         with BuildPart() as p:
             with BuildSketch():
-                RectangleRounded(bin_size, bin_size, radius=outer_rad)
+                RectangleRounded(my_bin_size, my_bin_size, radius=my_outer_rad)
             extrude(amount=plate_height)
             
             with BuildLine(mode=Mode.PRIVATE):
-                b = bin_size/2
+                b = my_bin_size/2
                 path = FilletPolyline((-b, -b), (-b, b), (b, b), (b, -b),
-                                    radius=outer_rad, close=True)
+                                    radius=my_outer_rad, close=True)
                 
             with BuildSketch(Plane(origin=path@0, z_dir=path%0).rotated((-90, 0, 0))):
                 with BuildLine():
@@ -32,4 +37,11 @@ class GFProfile(BasePartObject):
                 make_face()
             sweep(path=path, mode=Mode.SUBTRACT)
 
+            if not base:
+                Box(bin_size, bin_size, plate_base_height, mode=Mode.SUBTRACT, align=(Align.CENTER, Align.CENTER, Align.MIN))
+
         super().__init__(part=p.part, rotation=rotation, align=align, mode=mode)
+
+if (__name__ == "__main__"):
+    show_object(GFProfile(), "plate")
+    show_object(GFProfile(False), "bin")
